b7d7110c7a61e74e6050226e7dce6ab121924ca7
soenia <sophie.hopp@campus.lmu.de>
2025-06-16 14:07:40 +0200

    #6 create unit test for data

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,9 +1,29 @@
-from torch.utils.data import Dataset
+import os
+from datasets import load_from_disk
+from src.mlops_hatespeech.data import load_and_prepare_dataset
 
-from mlops_hatespeech.data import MyDataset
+def test_data(tmp_path):
+    """Test loading and preparing the dataset."""
 
+    load_and_prepare_dataset(split_val=0.2, seed=42, save_path=tmp_path)
+    assert os.path.exists(tmp_path)
+    dataset = load_from_disk(str(tmp_path))
 
-def test_my_dataset():
-    """Test the MyDataset class."""
-    dataset = MyDataset("data/raw")
-    assert isinstance(dataset, Dataset)
+    # Check expected splits
+    assert "train" in dataset
+    assert "validation" in dataset
+    assert "test" in dataset
+
+   # Check dataset sizes
+    assert len(dataset["train"]) > 0
+    assert len(dataset["validation"]) > 0
+    assert len(dataset["test"]) > 0
+
+    # Check a sample has expected fields
+    sample = dataset["train"][0]
+    assert "tweet" in sample
+    assert "label" in sample
+
+    # Check dataset types
+    assert dataset["train"].features["tweet"].dtype == "string"
+    assert dataset["train"].features["label"].dtype == "int64"
