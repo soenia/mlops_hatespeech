9a4defd5d74f52c97221084139ab05a55d83e181
alexanderulbrich <alex.ulbrich@gmx.net>
2025-06-16 14:06:46 +0200

    added commandline interface via typer to train script. Can also now be called by just typing train

--- src/mlops_hatespeech/train.py
+++ src/mlops_hatespeech/train.py
@@ -12,86 +12,91 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch
 import torch.nn as nn
-from model import MODEL_STR
+from mlops_hatespeech.model import MODEL_STR
+import typer
 
-ds = load_from_disk("data/processed")
+app = typer.Typer()
 
-DEVICE = "cpu"
+@app.command()
+def train(wd : float = 1e-3, lr : float = 2e-5, epochs : int = 5, seed : int = 42) -> None:
+    """Train a model."""
+    ds = load_from_disk("data/processed")
 
-idx2lbl = {
-    0: "non-hate",
-    1: "hate",
-}
-lbl2idx = {v: k for k, v in idx2lbl.items()}
+    DEVICE = "cpu"
 
-tokenizer = AutoTokenizer.from_pretrained(MODEL_STR)
-
-def tokenize_seqs(examples):
-    texts = examples["tweet"]
-    print(f"Type of examples['tweet']: {type(texts)}")
-    print(f"First example: {texts[0]}")
-    return tokenizer(texts, truncation=True, max_length=512)
-
-def is_valid(example):
-    text = example["tweet"]
-    return isinstance(text, str) and len(text.strip()) > 0
-
-ds = ds.filter(is_valid)
-
-ds = ds.map(tokenize_seqs, batched=True)
-
-ds = ds.rename_column("label", "labels")
-
-model = AutoModelForSequenceClassification.from_pretrained(
-    MODEL_STR,
-    num_labels=len(lbl2idx),
-    id2label=idx2lbl,
-    label2id=lbl2idx,
-)
-
-def compute_metrics(eval_preds):
-    logits, labels = eval_preds.predictions, eval_preds.label_ids
-    pred_labels = np.argmax(logits, axis=-1)
-    
-    f1 = f1_score(y_true=labels, y_pred=pred_labels, average="weighted")
-    acc = accuracy_score(y_true=labels, y_pred=pred_labels)
-    
-    return {
-        "f1": f1,
-        "accuracy": acc
+    idx2lbl = {
+        0: "non-hate",
+        1: "hate",
     }
+    lbl2idx = {v: k for k, v in idx2lbl.items()}
 
-training_args = TrainingArguments(
-    output_dir="./logs/run1",
-    per_device_train_batch_size=32,
-    per_gpu_eval_batch_size=128,
-    gradient_accumulation_steps=2,
-    learning_rate=2e-5,
-    weight_decay=1e-3,
-    num_train_epochs=5,
-    logging_strategy="steps",
-    logging_steps=100,
-    save_strategy="epoch",
-    eval_strategy="steps",
-    eval_steps=100,
-    save_total_limit=1,
-    seed=42,
-    data_seed=42,
-    dataloader_num_workers=0,
-    load_best_model_at_end=False,
-    report_to=None,
-    no_cuda=True
-)
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_STR)
 
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    compute_metrics=compute_metrics,
-    train_dataset=ds["train"],
-    eval_dataset=ds["validation"],
-    tokenizer=tokenizer,
-)
+    def tokenize_seqs(examples):
+        texts = examples["tweet"]
+        print(f"Type of examples['tweet']: {type(texts)}")
+        print(f"First example: {texts[0]}")
+        return tokenizer(texts, truncation=True, max_length=512)
+
+    def is_valid(example):
+        text = example["tweet"]
+        return isinstance(text, str) and len(text.strip()) > 0
+
+    ds = ds.filter(is_valid)
+    ds = ds.map(tokenize_seqs, batched=True)
+    ds = ds.rename_column("label", "labels")
+
+    model = AutoModelForSequenceClassification.from_pretrained(
+        MODEL_STR,
+        num_labels=len(lbl2idx),
+        id2label=idx2lbl,
+        label2id=lbl2idx,
+    )
+
+    def compute_metrics(eval_preds):
+        logits, labels = eval_preds.predictions, eval_preds.label_ids
+        pred_labels = np.argmax(logits, axis=-1)
+        
+        f1 = f1_score(y_true=labels, y_pred=pred_labels, average="weighted")
+        acc = accuracy_score(y_true=labels, y_pred=pred_labels)
+        
+        return {
+            "f1": f1,
+            "accuracy": acc
+        }
+
+    training_args = TrainingArguments(
+        output_dir="./logs/run1",
+        per_device_train_batch_size=32,
+        per_gpu_eval_batch_size=128,
+        gradient_accumulation_steps=2,
+        learning_rate=lr,
+        weight_decay=wd,
+        num_train_epochs=epochs,
+        logging_strategy="steps",
+        logging_steps=100,
+        save_strategy="epoch",
+        eval_strategy="steps",
+        eval_steps=100,
+        save_total_limit=1,
+        seed=seed,
+        data_seed=seed,
+        dataloader_num_workers=0,
+        load_best_model_at_end=False,
+        report_to=None,
+        no_cuda=True
+    )
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        compute_metrics=compute_metrics,
+        train_dataset=ds["train"],
+        eval_dataset=ds["validation"],
+        tokenizer=tokenizer,
+    )
+    trainer.train()
+    print("Training is done.")
 
 if __name__ == "__main__":
-    # Starte dein Training hier
-    trainer.train()
+    app()
